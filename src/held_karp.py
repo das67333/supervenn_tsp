0a26a46bd66265aa28d1e359d52756e780d430ad
das67333 <spkazimov@edu.hse.ru>
2023-05-15 19:22:39 +0300

    Discarded negative indices

--- src/held_karp.py
+++ src/held_karp.py
@@ -32,19 +32,17 @@ def held_karp(graph: np.array):
                         dp[v, comb_int] = dp[u, comb_int ^
                                              (1 << v)] + graph[u, v]
                         prev[v, comb_int] = u
-    len_best, v_last = DIST_MAX, -1
-    for k in range(k):
-        if len_best > dp[k, -1] + graph[k, n-1]:
-            len_best = dp[k, -1] + graph[k, n-1]
+    len_best, v_last, s_last = DIST_MAX, -1, (1 << (n-1)) - 1
+    for k in range(n-1):
+        if len_best > dp[k, s_last] + graph[k, n-1]:
+            len_best = dp[k, s_last] + graph[k, n-1]
             v_last = k
     assert v_last != -1
     path_best = [n-1]
-    s_last = (1 << (n-1)) - 1
-    while v_last != (n-1):
+    while v_last != n-1:
         path_best.append(v_last)
         s_last ^= 1 << v_last
         v_last = prev[v_last, s_last ^ (1 << v_last)]
-        assert v_last != -1
     return len_best, path_best
 
 
